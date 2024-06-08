44085e861a4110dcdca15b0bc2c8fe336838815d
dreadedbird <legionelite6@gmail.com>
2024-06-08 19:59:05 +0700

    fix flameshot dwm

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *flameshotcmd[]  = { "flameshot","gui", NULL };
+static const char *flameshotcmd[]  = { "flameshot", "gui", "--clipboard", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
